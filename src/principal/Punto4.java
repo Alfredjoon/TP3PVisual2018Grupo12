89cdb498e6b03fa26c823d43dba2fa5f0ac6d738
Alfred <Alfred@10.7.3.221>
2018-04-11 19:42:36 -0300

    Correccion punto 4 Alfred

--- src/principal/Punto4.java
+++ src/principal/Punto4.java
@@ -5,6 +5,7 @@
  */
 package principal;
 
+import dominio.ArregloNumeros;
 import java.util.Scanner;
 
 /**
@@ -22,25 +23,27 @@ public class Punto4 {
         
         
         Scanner scan = new Scanner (System.in);
-        int [] arregloEnteros = new int[10];
+        ArregloNumeros arregloEnteros = new ArregloNumeros();
         int maximo=0;
         int minimo=0;
         int suma=0;
+        int contador = 0;
         int promedio;
        
-        for (int i = 0 ; i<=arregloEnteros.length;i++){
+        while(contador<10){
         System.out.println("Ingrese numero al arreglo");
-        arregloEnteros[i]= scan.nextInt();
-        if (arregloEnteros[i]>maximo){
-            maximo=arregloEnteros[i];
-        }else if (arregloEnteros[i]< minimo){
-            minimo=arregloEnteros[i];}
-        suma=suma+arregloEnteros[i];
+        arregloEnteros.getNumeros()[contador]= scan.nextInt();
+        if (arregloEnteros.getNumeros()[contador]>maximo){
+            maximo=arregloEnteros.getNumeros()[contador];
+        }else if (arregloEnteros.getNumeros()[contador]< minimo){
+            minimo=arregloEnteros.getNumeros()[contador];}
+        suma=suma+arregloEnteros.getNumeros()[contador];
         }
         promedio=suma/10;
         System.out.println("el maximo numero es " +maximo);
         System.out.println("el minimo numero es " +minimo);
         System.out.println("el promedio de los numeros es " +promedio);
+        arregloEnteros.mostrarArreglo();
     }
     
 }
