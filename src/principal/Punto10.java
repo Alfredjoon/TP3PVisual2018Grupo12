96a114c5505b5c2894b3d5c20ed61e5ecd9660da
xd <>
2018-04-18 13:08:44 -0300

    prueba de subir

--- src/principal/Punto10.java
+++ src/principal/Punto10.java
@@ -16,6 +16,8 @@ public class Punto10 {
     /**
      * @param args the command line arguments
      */
+    
+    //Ramon es sin vistas, en consola tenemos que hacer la tabla
     public static void main(String[] args) {
         // TODO code application logic here
         VistaProducto panel = new VistaProducto();
